47a9f013d2d7a0b8e92457431d94d38046fd61bb
loinp <nploi1998@gmail.com>
2018-05-11 16:42:24 +0700

    Fix server

--- Server/Player.cpp
+++ Server/Player.cpp
@@ -11,8 +11,7 @@ string Player::receive() {
 	int check = recv(socket, str, sizeof(str), 0);
 	int lenght = strlen(str);
 	str[lenght] = NULL;
-	for (size_t i = 0; str[i] != NULL; i++)
-	{
+	for (size_t i = 0; str[i] != NULL; i++) {
 		result.push_back(str[i]);
 	}
 	if (check == SOCKET_ERROR) {
@@ -24,8 +23,7 @@ string Player::receive() {
 bool Player::sendAText(string text) {
 	int check = send(socket, text.c_str(), sizeof(text.c_str()), 0);
 
-	if (check != SOCKET_ERROR)
-	{
+	if (check != SOCKET_ERROR) {
 		return 1;
 	}
 	return 0;
